9db35aefa4a13a56d835baeb3382e8036e565aaa
Daniel Lewis <actiondan@gmail.com>
2019-05-08 19:01:12 +0100

    Do frozen fixes before imports (bundle dir & multiprocessing)

--- traumenc/__main__.py
+++ traumenc/__main__.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python
 import sys
 import multiprocessing
-from PyQt5.QtWidgets import QApplication
 
+if getattr(sys, 'frozen', False):
+    # if frozen, cd to the bundle dir
+    bundle_dir = sys._MEIPASS
+    os.chdir(bundle_dir)
+    # enable freeze support to avoid problems on windows
+    multiprocessing.freeze_support()
+
+from PyQt5.QtWidgets import QApplication
 from engine import create_engine
 from mainwindow import MainWindow
 from utils import setup_logging
@@ -11,14 +18,6 @@ import logging
 log = logging.getLogger('app')
 
 if __name__ == '__main__':
-    # enable frozen module support for Windows installer
-    multiprocessing.freeze_support()
-
-    # if frozen, cd to the bundle dir
-    if getattr(sys, 'frozen', False):
-        bundle_dir = sys._MEIPASS
-        os.chdir(bundle_dir)
-
     setup_logging(color=True)
     app = QApplication(sys.argv)
 
