61e335feeafe149c5b3cede62f314d21c55ef56a
Daniel Lewis <actiondan@gmail.com>
2019-05-08 16:12:58 +0100

    Add config.ini, disable upx and add --windowed for mac pyinstaller.

--- build-pyinstaller.sh
+++ build-pyinstaller.sh
@@ -1,1 +1,1 @@
-pyinstaller -y -n traumenc --add-data="icons;icons" --add-data="bin;bin" -p traumenc traumenc/__main__.py
+pyinstaller -y -n traumenc --add-data="config.ini:." --add-data="icons:icons" --add-data="bin:bin" --windowed --noupx -p traumenc traumenc/__main__.py
