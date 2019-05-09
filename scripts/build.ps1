580c9a37755f618035d7151e5e80ce89d9af90a8
Daniel Lewis <actiondan@gmail.com>
2019-05-09 14:06:54 +0100

    Add Powershell build script.

---
+++ scripts/build.ps1
@@ -0,0 +1,25 @@
+$appname = "traumenc"
+
+echo "Cleaning dist."
+Remove-Item -Recurse dist\$appname*
+
+echo "Building $appname with PyInstaller."
+
+pyinstaller -y `
+    --add-data="config.ini;." `
+    --add-data="icons;icons" `
+    --add-data="bin;bin" `
+    --windowed `
+    --noupx `
+    -p traumenc `
+    -n traumenc `
+    traumenc\__main__.py
+
+$build_id = (git rev-parse --short HEAD)
+$build_date = (Get-Date).tostring("yyMMdd")
+$build_basename = "${appname}-${build_date}-${build_id}"
+
+echo "Creating archive ${build_basename}.zip"
+Compress-Archive -Path dist\$appname -DestinationPath dist\$build_basename.zip
+
+echo "Build complete."
