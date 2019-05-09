ba0f2cae53bd6b321fc09d18a885263f9825dac9
Daniel Lewis <actiondan@gmail.com>
2019-05-09 21:51:57 +0100

    Add architecture to build names.

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,5 +1,10 @@
 $appname = "traumenc"
 
+$build_arch = "win64"
+$build_id = (git rev-parse --short HEAD)
+$build_date = (Get-Date).tostring("yyMMdd")
+$build_basename = "${appname}-${build_date}-${build_id}-${build_arch}"
+
 echo "Cleaning dist."
 Remove-Item -Recurse dist\$appname*
 
@@ -14,10 +19,6 @@ pyinstaller -y `
     -p $appname `
     -n $appname `
     $appname\__main__.py
-
-$build_id = (git rev-parse --short HEAD)
-$build_date = (Get-Date).tostring("yyMMdd")
-$build_basename = "${appname}-${build_date}-${build_id}"
 
 echo "Creating archive ${build_basename}.zip"
 Compress-Archive -Path dist\$appname -DestinationPath dist\$build_basename.zip
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,20 @@
 appname=traumenc
 
+uname=`uname`
+if [ $uname = 'Darwin' ]; then
+    build_arch=mac64
+elif [ $uname = 'Linux' ]; then
+    build_arch=lnx64
+else
+    echo "Unknown platform '$uname'."
+    exit 1
+fi
+
+build_id=`git rev-parse --short HEAD`
+build_date=`date +%y%m%d`
+build_basename=${appname}-${build_date}-${build_id}-${build_arch}
+
+
 echo "Cleaning dist."
 (cd dist &&
     rm -rf ${appname}*)
@@ -17,11 +32,7 @@ pyinstaller -y \
     -n $appname \
     $appname/__main__.py
 
-build_id=`git rev-parse --short HEAD`
-build_date=`date +%y%m%d`
-build_basename=${appname}-${build_date}-${build_id}
-
-if [ $(uname) = 'Darwin' ]; then
+if [ $uname = 'Darwin' ]; then
     echo "Creating disk image ${build_basename}.dmg"
     (cd dist &&
         mkdir ${build_basename} &&
